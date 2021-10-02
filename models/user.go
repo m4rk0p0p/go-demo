da6d9949765953750bce451dd347c86af796a662
Marko Popovic <m4rk0p0p@gmail.com>
2021-10-02 07:19:33 +0200

    Add a simple model for a user

---
+++ models/user.go
@@ -0,0 +1,62 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+)
+
+type User struct {
+	Id        int
+	FirstName string
+	LastName  string
+}
+
+var (
+	users  []*User
+	nextId = 1
+)
+
+func GetUser() []*User {
+	return users
+}
+
+func AddUser(usr User) (User, error) {
+	if usr.Id == 0 {
+		return User{}, errors.New("New user must have ID zero")
+	}
+	usr.Id = nextId
+	nextId++
+
+	users = append(users, &usr)
+
+	return usr, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, usr := range users {
+		if usr.Id == id {
+			return *usr, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' was not found", id)
+}
+
+func UpdateUser(usr User) (User, error) {
+	for i, currUsr := range users {
+		if usr.Id == currUsr.Id {
+			users[i] = &usr
+			return usr, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with ID '%v' could not be updated", id)
+}
+
+func RemoveUserById(id int) error {
+	for i, usr := range users {
+		if usr.Id == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with ID '%v' was not found for removal", id)
+}
